7392639dba7fd742c8dd9bba14ab8284e1e206db
Rodionchik73 <olelik260591@gmail.com>
2025-01-25 23:27:29 +0300

    added test seleniumList

--- src/test/java/org/example/demo4/MainPageTest.java
+++ src/test/java/org/example/demo4/MainPageTest.java
@@ -1,15 +1,20 @@
 package org.example.demo4;
 
-import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainPageTest {
     private WebDriver driver;
@@ -29,15 +34,30 @@ public class MainPageTest {
     }
 
     @Test
-    public void search() {
-String input = "Selenium";
+    public void seleniumList() {
+        String input = "Selenium";
         WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
         searchField.sendKeys(input);
         searchField.submit();
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.attributeContains(By.cssSelector("h2 > a[href]"), "href", "selenium"),
+                ExpectedConditions.elementToBeClickable(By.cssSelector("h2 > a[href]"))
+        ));
+        List results = driver.findElements(By.cssSelector("h2 > a[href]"));
+        clickElement(results, 0);
+        driver.getCurrentUrl();
+        ArrayList tabs = new ArrayList<>(driver.getWindowHandles());
+        if (tabs.size() > 1) driver.switchTo().window(tabs.get(1).toString());
 
-        WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
-        assertEquals(input, searchPageField.getAttribute("value"));
+        assertEquals("https://www.selenium.dev/", driver.getCurrentUrl(), "Первый отображаемый сайт на странице https://www.selenium.dev/");
+
+    }
+
+    public void clickElement(List<WebElement> results, int num) {
+        results.get(num).click();
+        System.out.println("Клик по " + num + " при поиске Selenium");
     }
 
 }
